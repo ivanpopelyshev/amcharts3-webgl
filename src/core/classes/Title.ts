236033ee5040327c812362ef7df56305004460fe
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2021-11-25 12:07:10 +0300

    use DefinitelyTyped

---
+++ src/core/classes/Title.ts
@@ -0,0 +1,30 @@
+/* eslint-disable */
+import { BaseClass } from '../BaseClass';
+
+export class Title extends BaseClass {
+    /**
+     * @default 1
+     */
+    alpha: number;
+    /**
+     * Specifies if the tile is bold or not.
+     * @default false
+     */
+    bold: boolean;
+    /**
+     * Text color of a title.
+     */
+    color: string;
+    /**
+     * Unique id of a Title. You don't need to set it, unless you want to.
+     */
+    id: string;
+    /**
+     * Text size
+     */
+    size: number;
+    /**
+     * Text of a label
+     */
+    text: string;
+}
